7c333e7366ba64fe63c3ab34ee9b9127eeaf8513
CmdrAdama <cmdradmladama@gmail.com>
2017-09-21 23:48:35 -0500

    Added Modesl for Guardian and fetch from the api to the controller.
    Added exception handling check for error 500 returned when the api is sent an invalid ID.

--- Heimdallr/Controllers/LookupController.cs
+++ Heimdallr/Controllers/LookupController.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Heimdallr.Settings;
+using Heimdallr.Models;
 using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Diagnostics;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace Heimdallr.Controllers
 {
@@ -36,9 +40,31 @@ namespace Heimdallr.Controllers
         [Route("/Lookup/GR{siteNum:int}")]
         public IActionResult Guardian(int siteNum)
         {
-            ViewData["Message"] = _guardianRuinSettings.Value.resourceLocation ;
 
-            return View("Guardian");
+            string guardianJSON = "";
+            try
+            {
+                guardianJSON = Task.Run(() =>
+                {
+                    return GetUrlContents(_guardianRuinSettings.Value.resourceLocation + "api/v1/maps/ruininfo/" + siteNum);
+                }).Result;
+            }
+            catch (Exception ex)
+            {
+                Exception asyncEx = ex.GetBaseException();
+                //Error thrown from the backend api such as a 500 when we send an invalid ID?
+                if (asyncEx is HttpRequestException){
+                    throw new Exception("Unable to retrieve the specified Guardian Ruin.  Please check that the Ruin ID is correct.");
+                }
+
+                //No.  Log it
+                //TODO: Log exception here
+                throw new Exception("Unexpected error when trying to retrieve Guardian Ruin");
+            }
+
+            GuardianRuin ruinInfo = JsonConvert.DeserializeObject<GuardianRuin>(guardianJSON);
+
+            return View("Guardian", ruinInfo);
         }
 
 
@@ -63,5 +89,22 @@ namespace Heimdallr.Controllers
             var feature = this.HttpContext.Features.Get<IExceptionHandlerFeature>();
             return View("Error",feature.Error);
         }
+
+        private static async Task<string> GetUrlContents(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage responseMsg = await client.GetAsync(url))
+                {
+                    responseMsg.EnsureSuccessStatusCode();
+                    return responseMsg.Content.ReadAsStringAsync().Result;
+                }
+                
+
+            }
+
+            
+        }
+
     }
 }
---
+++ Heimdallr/Models/GuardianRuin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Heimdallr.Models
+{
+    public class GuardianRuin
+    {
+        public int RuinId { get; set; }
+        public string SystemName { get; set; }
+        public string BodyName { get; set; }
+
+        public string RuinTypeName { get; set; }
+        public string EdsmSystemLink { get; set; }
+        public string EdsmBodyLink { get; set; }
+
+        public decimal[] Coordinates { get; set; }
+    }
+}
